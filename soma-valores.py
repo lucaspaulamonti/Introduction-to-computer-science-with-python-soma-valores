946d0ac780cba3073dd346359222e40f91968e2c
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-25 14:13:59 -0300

    Create soma-valores.py

---
+++ soma-valores.py
@@ -0,0 +1,6 @@
+# Crie um programa em python que realize a soma de valores a partir de uma função.
+def soma(x,y):
+    return x+y
+print(soma(10,20))
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
